93599f9c30d91787b1c5cf4c273adc4b4b90cd86
Zac Delventhal <delventhalz@gmail.com>
2017-08-16 13:30:03 -0500

    Add "create" action to handlers
    Signed-off-by: Zac Delventhal <delventhalz@gmail.com>

--- processor/handlers.js
+++ processor/handlers.js
@@ -3,9 +3,24 @@
 const { createHash } = require('crypto')
 const { TransactionHandler } = require('sawtooth-sdk/processor')
 const { InvalidTransaction } = require('sawtooth-sdk/processor/exceptions')
+const { TransactionHeader } = require('sawtooth-sdk/protobuf')
+
+// Encoding helpers and constants
+const getAddress = (key, length = 64) => {
+  return createHash('sha512').update(key).digest('hex').slice(0, length)
+}
 
 const FAMILY = 'transfer-chain'
-const PREFIX = createHash('sha512').update(FAMILY).digest('hex').slice(0, 6)
+const PREFIX = getAddress(FAMILY, 6)
+
+const getAssetAddress = name => PREFIX + '00' + getAddress(name, 62)
+const encode = obj => Buffer.from(JSON.stringify(obj, Object.keys(obj).sort()))
+
+// Add a new asset to state
+const createAsset = (asset, owner, state) => {
+  const address = getAssetAddress(asset)
+  return state.set({[address]: encode({name: asset, owner})})
+}
 
 // Handler for JSON encoded payloads
 class JSONHandler extends TransactionHandler {
@@ -14,9 +29,17 @@ class JSONHandler extends TransactionHandler {
   }
 
   apply (txn, state) {
+    // Parse the transaction header and payload
+    const header = TransactionHeader.decode(txn.header)
+    const signer = header.signerPubkey
+    const { action, asset } = JSON.parse(txn.payload)
+
+    // Call the appropriate function based on the payload's action
+    if (action === 'create') return createAsset(asset, signer, state)
+
     return Promise.resolve().then(() => {
       throw new InvalidTransaction(
-        "I don't know how to handle any transactions!"
+        'Action must be "create"'
       )
     })
   }
