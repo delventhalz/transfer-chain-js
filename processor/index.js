cbfd6da0787a7f6c5acb9585f4185fd4eaba0193
Zac Delventhal <delventhalz@gmail.com>
2017-08-16 13:20:18 -0500

    Add basic Transaction Processor with no handler
    Signed-off-by: Zac Delventhal <delventhalz@gmail.com>

--- processor/index.js
+++ processor/index.js
@@ -1,1 +1,8 @@
-console.log('Hello Processor!')
+'use strict'
+
+const { TransactionProcessor } = require('sawtooth-sdk/processor')
+const VALIDATOR_URL = 'tcp://localhost:4004'
+
+// Initialize Transaction Processor
+const tp = new TransactionProcessor(VALIDATOR_URL)
+tp.start()
