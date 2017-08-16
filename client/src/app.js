a73b22c0724be39b6a5ebc68158aa35140692ef5
Zac Delventhal <delventhalz@gmail.com>
2017-08-16 14:04:27 -0500

    Add blockchain state fetching to client
    Signed-off-by: Zac Delventhal <delventhalz@gmail.com>

--- client/src/app.js
+++ client/src/app.js
@@ -1,1 +1,5 @@
-console.log('Hello Client!')
+'use strict'
+
+const { getState } = require('./state')
+
+getState(data => console.log(data))
---
+++ client/src/state.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const $ = require('jquery')
+
+// Config variables
+const API_URL = 'http://localhost:8080'
+const PREFIX = '19d832'
+
+// Fetch current Transfer Chain state from validator
+const getState = cb => {
+  $.get(`${API_URL}/state?address=${PREFIX}`, ({ data }) => {
+    cb(data.reduce((processed, datum) => {
+      if (datum.data !== '') {
+        const parsed = JSON.parse(atob(datum.data))
+        if (datum.address[7] === '0') processed.assets.push(parsed)
+        if (datum.address[7] === '1') processed.transfers.push(parsed)
+      }
+      return processed
+    }, {assets: [], transfers: []}))
+  })
+}
+
+module.exports = {
+  getState
+}
